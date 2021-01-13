2a2e107a6bd512c7a5202f213b28d0a28f0a062a
randomlylelo <lzhang.0693@gmail.com>
2021-01-12 20:28:57 -0500

    created bubble sort in ts!

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
 const testarr = [2, 3, 5, 6, 1, 23, 45, 643, 12, 324];
 
-
+// Using TS functions
 function sortNumArray(arr: number[]): Array<number> {
   return arr.sort((a, b) => a - b);
 }
 
-console.log(sortNumArray(testarr))
+console.log(sortNumArray(testarr));
+
+
+// Bubble sort
+function sortNumArray2(arr: number[]): number[] {
+  let sortedArr: number[] = arr; // Copy the array
+
+  const swap = (array: number[], firIndex: number, secIndex: number): number[] => {
+    let temp = array[firIndex];
+    array[firIndex] = array[secIndex];
+    array[secIndex] = temp;
+    return array;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if(sortedArr[j] > sortedArr[j+1]) {
+        sortedArr = swap(sortedArr, j, j+1)
+      }
+    }
+  }
+
+  return sortedArr;
+}
+
+console.log(sortNumArray2(testarr))
